5e83f50fe4c06b7d5cc4b00291ca806078b8d555
emap-strawberry <melon.pan.san@gmail.com>
2020-12-17 17:23:43 +0900

    sample.pyのpythonのversion指定を削除

--- sample.py
+++ sample.py
@@ -1,8 +1,4 @@
-from sys import version_info
-if version_info.major == 2:
-    import Tkinter as tk
-else:
-    import tkinter as tk
+import tkinter as tk
     
 app = tk.Tk()
 app.title("Hello World")
